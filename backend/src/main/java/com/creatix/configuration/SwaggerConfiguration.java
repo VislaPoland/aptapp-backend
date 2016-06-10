5d45f3ea268bd916b506b97bfd68da9a6502b265
Tomas Sedlak <tomas.sedlak@masterminds.sk>
2016-06-10 23:39:21 +0200

    Update tests to latest spring-test framework.

--- backend/src/main/java/com/creatix/configuration/SwaggerConfiguration.java
+++ backend/src/main/java/com/creatix/configuration/SwaggerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,7 +30,7 @@ public class SwaggerConfiguration {
         return new ApiInfoBuilder()
                 .title("Gym Time")
                 .description("Gym Time application backend")
-                .contact("Tomas Sedlak")
+                .contact(new Contact("Tomas Sedlak", "http://thinkcreatix.com/", "tomas.sedlak@thinkcratix.com"))
                 .version("0.0.1")
                 .build();
     }
