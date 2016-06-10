0fad313b46d3200e826e594eabdea8b84aeb8496
Tomas Sedlak <tomas.sedlak@thinkcreatix.com>
2016-06-10 10:46:48 +0200

    Add example authenticated account retrieval.

--- backend/src/main/java/com/creatix/controller/HelloWorldController.java
+++ backend/src/main/java/com/creatix/controller/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.creatix.controller;
 
 import com.creatix.domain.dto.DataResponse;
+import com.creatix.security.AuthorizationManager;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/hello")
 class HelloWorldController {
 
+    @Autowired
+    private AuthorizationManager authorizationManager;
+
     @ApiOperation(value = "hello world")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success"),
@@ -22,7 +27,12 @@ class HelloWorldController {
             @ApiResponse(code = 404, message = "Not found")})
     @RequestMapping(value = "/world", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public DataResponse<String> getEmployers() {
-        return new DataResponse<>("Hello world!");
+        if ( authorizationManager.hasCurrentAccount() ) {
+            return new DataResponse<>("Hello world! You are logged in as " + authorizationManager.getCurrentAccount().getEmail());
+        }
+        else {
+            return new DataResponse<>("Hello world!");
+        }
     }
 
 }
