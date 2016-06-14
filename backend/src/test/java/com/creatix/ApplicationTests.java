fde731fc686768c90201bf60cb75adf3036fb425
Tomas Sedlak <tomas.sedlak@thinkcreatix.com>
2016-06-14 18:44:34 +0200

    Add WAR building support.

--- backend/src/test/java/com/creatix/ApplicationTests.java
+++ backend/src/test/java/com/creatix/ApplicationTests.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest(classes = AptAppBackendApplication.class)
-@ActiveProfiles("test")
+@ActiveProfiles(TestContext.PROFILE)
 @Transactional
 @Component
 public class ApplicationTests extends TestContext {
--- backend/src/test/java/com/creatix/TestContext.java
+++ backend/src/test/java/com/creatix/TestContext.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -14,11 +15,14 @@ import javax.persistence.PersistenceContext;
 import java.util.TimeZone;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest(classes = AptAppBackendApplication.class)
-@ActiveProfiles("test")
+@ActiveProfiles(TestContext.PROFILE)
 @Transactional
 @Component
 public abstract class TestContext {
+
+    public static final String PROFILE = "test";
 
     @PersistenceContext
     protected EntityManager em;
