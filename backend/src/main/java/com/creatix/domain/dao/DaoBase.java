eb9f68aaa3992d942b6548b9c70f27176a80a3c6
Andrej Kosar <andrejksr@gmail.com>
2016-06-15 13:49:07 +0200

    added findById in DaoBase

--- backend/src/main/java/com/creatix/domain/dao/DaoBase.java
+++ backend/src/main/java/com/creatix/domain/dao/DaoBase.java
@@ -1,10 +1,14 @@
 package com.creatix.domain.dao;
 
+import org.springframework.core.GenericTypeResolver;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 /**
  * Base class for all repository classes.
@@ -12,9 +16,18 @@ import javax.persistence.PersistenceContext;
 @Repository
 @Transactional
 class DaoBase<T, ID> {
-
     @PersistenceContext
     EntityManager em;
+
+    Class<T> type;
+
+    DaoBase() {
+        this.type = (Class<T>) GenericTypeResolver.resolveTypeArgument(getClass(), DaoBase.class);
+    }
+
+    public T findById(ID id) {
+        return em.find(type, id);
+    }
 
     public void persist(T entity) {
         em.persist(entity);
