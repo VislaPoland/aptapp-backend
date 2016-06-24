2a9e08b1ecae2b511c343d3e0d5d843c4ec992a7
Tomas Sedlak <tomas.sedlak@thinkcreatix.com>
2016-06-24 09:39:54 +0200

    Convert jpql query to querydsl.

--- backend/src/main/java/com/creatix/domain/dao/NotificationDao.java
+++ backend/src/main/java/com/creatix/domain/dao/NotificationDao.java
@@ -3,6 +3,7 @@ package com.creatix.domain.dao;
 import com.creatix.domain.entity.MaintenanceNotification;
 import com.creatix.domain.entity.Notification;
 import com.creatix.domain.entity.QMaintenanceNotification;
+import com.creatix.domain.entity.QNotification;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,16 +13,18 @@ import java.util.List;
 @Repository
 @Transactional
 public class NotificationDao extends AbstractNotificationDao<Notification> {
+
     public List<Notification> findAllInDateRange(Date fromDate, Date tillDate) {
-        return em.createQuery("SELECT n FROM Notification n WHERE n.date BETWEEN :fromDate AND :tillDate", Notification.class)
-                .setParameter("fromDate", fromDate)
-                .setParameter("tillDate", tillDate)
-                .getResultList();
+        final QNotification notification = QNotification.notification;
+        return queryFactory.selectFrom(notification)
+                .where(notification.date.between(fromDate, tillDate))
+                .fetch();
     }
 
     public List<MaintenanceNotification> findAllMaintenanceInDateRange(Date fromDate, Date tillDate) {
-        return queryFactory.selectFrom(QMaintenanceNotification.maintenanceNotification)
-                .where(QMaintenanceNotification.maintenanceNotification.date.between(fromDate, tillDate))
+        final QMaintenanceNotification maintenanceNotification = QMaintenanceNotification.maintenanceNotification;
+        return queryFactory.selectFrom(maintenanceNotification)
+                .where(maintenanceNotification.date.between(fromDate, tillDate))
                 .fetch();
     }
 }
