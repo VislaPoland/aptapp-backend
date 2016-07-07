5276036055bcf9324cf10030f0770df89ac7fd31
Tomas Sedlak <tomas.sedlak@thinkcreatix.com>
2016-07-07 15:33:54 +0200

    Add title and description to event slot.

--- backend/src/main/java/com/creatix/domain/dto/property/slot/EventSlotDto.java
+++ backend/src/main/java/com/creatix/domain/dto/property/slot/EventSlotDto.java
@@ -1,6 +1,7 @@
 package com.creatix.domain.dto.property.slot;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,4 +9,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class EventSlotDto extends SlotDto {
+    @ApiModelProperty(value = "Title", required = true)
+    private String title;
+    @ApiModelProperty(value = "Description")
+    private String description;
 }
--- backend/src/main/java/com/creatix/domain/entity/store/EventSlot.java
+++ backend/src/main/java/com/creatix/domain/entity/store/EventSlot.java
@@ -4,7 +4,9 @@ package com.creatix.domain.entity.store;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
@@ -12,4 +14,9 @@ import javax.persistence.Entity;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class EventSlot extends Slot {
+    @NotEmpty
+    @Column
+    private String title;
+    @Column
+    private String description;
 }
