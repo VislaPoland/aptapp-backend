0e871c02d5de3988ad30fe909896d9777ddb9293
Tomas Sedlak <tomas.sedlak@thinkcreatix.com>
2016-08-17 15:07:30 +0200

    Inactive and deleted accounts are not allowed to login.

--- backend/src/main/java/com/creatix/security/AuthenticatedUserDetailsService.java
+++ backend/src/main/java/com/creatix/security/AuthenticatedUserDetailsService.java
@@ -3,6 +3,9 @@ package com.creatix.security;
 import com.creatix.domain.dao.AccountDao;
 import com.creatix.domain.entity.store.account.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,12 +18,19 @@ public class AuthenticatedUserDetailsService implements UserDetailsService {
     private AccountDao accountDao;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws AuthenticationException {
         final Account account = this.accountDao.findByEmail(username);
 
-        if ((account == null)) {
+        if ( account == null ) {
             throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
-        } else {
+        }
+        else if ( account.getActive() == Boolean.FALSE ) {
+            throw new DisabledException(String.format("Account %s is not activated", username));
+        }
+        else if ( account.getDeletedAt() != null ) {
+            throw new AccountExpiredException(String.format("Account %s is deleted", username));
+        }
+        else {
             return new AuthenticatedUserDetails(account);
         }
     }
