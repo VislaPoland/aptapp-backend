f89f745ae750638bb9d5528ee61a025922eba416
Tomas Michalek <tomas.michalek@thinkcreatix.com>
2017-05-29 09:34:22 +0200

    APT-165 Personal message service layer

---
+++ backend/src/main/java/com/creatix/domain/dto/notification/PersonalMessageDto.java
@@ -0,0 +1,12 @@
+package com.creatix.domain.dto.notification;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * Created by Tomas Michalek on 26/05/2017.
+ */
+@Data
+@ApiModel("Personal message")
+public class PersonalMessageDto {
+}
---
+++ backend/src/main/java/com/creatix/domain/entity/store/notification/PersonalMessageNotification.java
@@ -0,0 +1,32 @@
+package com.creatix.domain.entity.store.notification;
+
+import com.creatix.domain.enums.NotificationType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by kvimbi on 29/05/2017.
+ */
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PersonalMessageNotification extends Notification {
+
+    @ManyToOne
+    private PersonalMessage personalMessage;
+
+    public PersonalMessageNotification() {
+        this.setType(NotificationType.PersonalMessage);
+    }
+
+    @Override
+    public void setType(NotificationType type) {}
+
+    @Override
+    public NotificationType getType() {
+        return NotificationType.PersonalMessage;
+    }
+}
