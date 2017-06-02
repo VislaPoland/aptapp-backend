585c20487c8630a99f3d867b053908272adb0902
Tomas Michalek <tomas.michalek@thinkcreatix.com>
2017-06-02 10:58:27 +0200

    Added list of community board categories

--- backend/src/main/java/com/creatix/domain/dao/community/board/CommunityBoardCategoryDao.java
+++ backend/src/main/java/com/creatix/domain/dao/community/board/CommunityBoardCategoryDao.java
@@ -2,9 +2,11 @@ package com.creatix.domain.dao.community.board;
 
 import com.creatix.domain.dao.DaoBase;
 import com.creatix.domain.entity.store.community.board.CommunityBoardCategory;
+import com.creatix.domain.entity.store.community.board.QCommunityBoardCategory;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by Tomas Michalek on 11/05/2017.
@@ -12,4 +14,7 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public class CommunityBoardCategoryDao extends DaoBase<CommunityBoardCategory, Long> {
+    public List<CommunityBoardCategory> listAll() {
+        return queryFactory.selectFrom(QCommunityBoardCategory.communityBoardCategory).fetch();
+    }
 }
