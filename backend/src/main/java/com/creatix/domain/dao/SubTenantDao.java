07241856bc2200873ef03487799583fa1b8ce6b9
lubos.zlatohlavek <lubos.zlatohlavek@smartbase.sk>
2018-07-04 14:01:29 +0200

    Removed unused imports

--- backend/src/main/java/com/creatix/domain/dao/SubTenantDao.java
+++ backend/src/main/java/com/creatix/domain/dao/SubTenantDao.java
@@ -1,11 +1,8 @@
 package com.creatix.domain.dao;
 
 import com.creatix.domain.entity.store.account.SubTenant;
-import com.creatix.domain.entity.store.account.Tenant;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 @Transactional
